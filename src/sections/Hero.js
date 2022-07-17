39d2065deeb7d1c009bebf3e6476af08cd9f8c5e
wibawasuyadnya <dekgusnfs@gmail.com>
2022-07-17 20:47:46 +0800

    breakpoint test

--- src/sections/Hero.js
+++ src/sections/Hero.js
@@ -11,7 +11,7 @@ function Hero ({name}) {
                 <img src={images} width="200px" className="border-solid border-2 border-slate-900 rounded-full m-auto"/>
                 </div> 
                 <div className="">
-                <h1 className="text-center text-slate-900 font-bold text-5xl tracking-wider font-montserrat py-5">Hey I'm {name}</h1>
+                <h1 className="text-center text-slate-900 font-bold text-5xl tracking-wider font-montserrat py-5 mobile:text-xs">Hey I'm {name}</h1>
                 <h2 className="text-center font-normal text-4xl tracking-wider">Web Designer | Front-end <br />Developer | UI & UX <br />Designer</h2>
                 </div>
                 <div className="h-64 -mb-40 mt-16 flex">
