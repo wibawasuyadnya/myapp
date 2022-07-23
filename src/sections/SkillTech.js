1ccd91b86e0479e330f2534857bf86ae8d432de0
iwankrisnawan <iwankrisnawan03@gmail.com>
2022-07-23 22:05:24 +0800

    fix img

--- src/sections/SkillTech.js
+++ src/sections/SkillTech.js
@@ -1,15 +1,12 @@
 import React from "react";
 
 const SkillTech = ({ sname, sicon }) => {
-   return (
-   <>
-   <div className="m-4 w-40 flex-none mx-auto text-center p-5 rounded-xl border-2 border-gray-300">
-      {sicon}
-      <p className="text-xl font-semibold mt-4">{sname}</p>
-    </div>
-    </>
-   );
+  return (
+      <div className="m-4 w-40 flex-none mx-auto text-center p-5 rounded-xl border-2 border-gray-300">
+        {sicon}
+        <p className="text-xl font-semibold mt-4">{sname}</p>
+      </div>
+  );
 };
-
 
 export default SkillTech;
--- src/sections/Tools.js
+++ src/sections/Tools.js
@@ -1,26 +1,26 @@
 import React from "react";
 import SkillTech from "./SkillTech";
 
-function Tools({title, tech}){
+function Tools({ title, tech }) {
   return (
- <>
+    <>
       <div className="container min-h-[400px] mx-auto">
         <div>
           <h2 className="text-center text-[40px] font-bold">{title}</h2>
         </div>
         <div className="flex flex-col sm:flex-row align-center justify-center max-w-2xl mx-auto mt-8">
-      {tech.map((skills, index) => {
-        return (
-          <SkillTech 
-          key={index} 
-          skillName={skills.skillName} 
-          skillIcon={skills.skillIcon}
-          />
-        );
-      })}
-    </div>
+          {tech.map((skills, index) => {
+            return (
+              <SkillTech
+                key={index}
+                sname={skills.skillName}
+                sicon={skills.skillIcon}
+              />
+            );
+          })}
+        </div>
       </div>
-  </>
+    </>
   );
 }
 
