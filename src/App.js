3702fb28df7a63d517a5a9ffd66f0067e69b25d5
wibawasuyadnya <dekgusnfs@gmail.com>
2022-07-25 23:37:58 +0800

    create link for each tool

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Hero from './sections/Hero';
 import About from './sections/About';
 import Tools from './sections/Tools.js';
 import data from './assets/data';
+import Nav from './sections/sidebar/Sidebar';
+
 import { isCompositeComponent } from 'react-dom/test-utils';
 
 
@@ -10,7 +12,7 @@ function App() {
   return (
     <>
     <header/>
-
+    <Nav />
     <main>
       <Hero name={data.hero.name} desc={data.hero.desc} medsos={data.hero.social} resume={data.hero.resume} contact={data.hero.contact}></Hero>
       <About desc={data.about.desc}></About>
